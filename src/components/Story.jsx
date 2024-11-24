b5d26fe73e1bd0688ba04dbf955472efb646b9c2
Nikoloz Tsutskiridze <nikacucqiridze2000@gmail.com>
2024-11-24 18:31:25 +0400

    Story

---
+++ src/components/Story.jsx
@@ -0,0 +1,100 @@
+import gsap from "gsap";
+import { useRef } from "react";
+
+import Button from "./Button";
+import AnimatedTitle from "./AnimatedTitle";
+
+const FloatingImage = () => {
+  const frameRef = useRef(null);
+
+  const handleMouseMove = (e) => {
+    const { clientX, clientY } = e;
+    const element = frameRef.current;
+
+    if (!element) return;
+
+    const rect = element.getBoundingClientRect();
+    const xPos = clientX - rect.left;
+    const yPos = clientY - rect.top;
+
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+
+    const rotateX = ((yPos - centerY) / centerY) * 10;
+    const rotateY = ((xPos - centerX) / centerX) * 10;
+
+    gsap.to(element, {
+      duration: 0.3,
+      rotateX,
+      rotateY,
+      transformPerspective: 500,
+      ease: "power1.inOut",
+    });
+  };
+
+  const handleMouseLeave = () => {
+    const element = frameRef.current;
+
+    if (element) {
+      gsap.to(element, {
+        duration: 0.3,
+        rotateX: 0,
+        rotateY: 0,
+        ease: "power1.inOut",
+      });
+    }
+  };
+
+  return (
+    <div id="story" className="min-h-dvh w-screen bg-black text-blue-50">
+      <div className="flex size-full flex-col items-center pt-10 pb-24">
+        <p className="font-general text-sm uppercase md:text-[10px]">
+          Rainbow Six Siege: Operation Black Ice
+        </p>
+
+        <div className="relative size-full">
+          <AnimatedTitle
+            title="the bl<b>a</b>ck ice <br />operatio<b>n</b>"
+            containerClass="mt-5 pointer-events-none mix-blend-difference relative z-10 "
+          />
+
+          <div className="story-img-container">
+            <div className="story-img-mask">
+              <div className="story-img-content">
+                <img
+                  ref={frameRef}
+                  onMouseMove={handleMouseMove}
+                  onMouseLeave={handleMouseLeave}
+                  onMouseUp={handleMouseLeave}
+                  onMouseEnter={handleMouseLeave}
+                  src="/img/story.jpg"
+                  alt="black ice operation"
+                  className="object-contain"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="-mt-80 flex w-full justify-center md:-mt-64 md:me-44 md:justify-end">
+          <div className="flex h-full w-fit flex-col items-center md:items-start">
+            <p className="mt-3 max-w-sm text-center font-circular-web text-violet-50 md:text-start">
+              In the Black Ice operation, elite operators take on one of the
+              most dangerous tactical challenges of their careers. Engage in icy
+              environments with extreme weather, breaking barriers with skill,
+              precision, and the latest tech.
+            </p>
+
+            <Button
+              id="realm-btn"
+              title="Enter the Operation"
+              containerClass="mt-5"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FloatingImage;
